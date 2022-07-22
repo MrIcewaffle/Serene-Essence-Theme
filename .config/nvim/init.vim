3402a344deaef26e198279137549b91b72320436
MrIcewaffle <mricewaffle@gmail.com>
2022-07-22 19:38:20 +1000

    added nvim + alacritty configs, added font + dots copy to deploy.sh

---
+++ .config/nvim/init.vim
@@ -0,0 +1,11 @@
+set number
+set cc=80
+set cursorline
+syntax on
+"set backupdir=~/.cache/vim
+
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+set autoindent
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+wrkdir=${PWD}
 baskergit="https://github.com/baskerville"
 
 ##install using 'assume-yes'
@@ -8,6 +9,7 @@ install() {
 }
 
 installbspdeps() {
+    echo "Installing dependencies..."
     apt --assume-yes install libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev
 }
 
@@ -17,6 +19,7 @@ maketempdir() {
 }
 
 clonerepo() {
+    echo "Cloning $1..."
     reponame="$1"
     git clone "$baskergit/${reponame%.git}" "$repodir/$reponame"
 }
@@ -26,37 +29,49 @@ gitmakeinstall() {
     progpath="$repodir/$progname/"
 
     cd "$progpath" || exit 1
-    make >/dev/null 2>&1
-    make install >/dev/null 2>&1
+    make
+    make install
     cd /tmp || return 1
 }
 
+copyfonts() {
+    echo "Copying fonts..."
+    mkdir ${HOME}/.local/share/fonts
+    cp -r "$wrkdir/.fonts/*" ${HOME}/.local/share/fonts/
+    fc-cache -f -v
+}
+
+copydots() {
+    echo "Copying Wallpaper & .dot files..."
+    cp -r "$wrkdir/Wallpaper" "${HOME}"
+    cp -r "$wrkdir/.config" "${HOME}"
+    cp -r "$wrkdir/.scripts" "${HOME}"
+}
+
 cleanup() {
+    echo "Cleaning up $repodir..."
     rm -rf "$repodir"
 }
 
 ##run script
+#install
 maketempdir
-
 installbspdeps
-
 clonerepo "bspwm"
-
 clonerepo "sxhkd"
-
 gitmakeinstall "bspwm"
-
 gitmakeinstall "sxhkd"
-
 install "alacritty"
-
 install "neovim"
-
 install "rofi"
-
 install "feh"
-
 install "picom"
-
 install "polybar"
 
+#copy configs from repo
+copyfonts
+copydots
+
+#cleanup and exit
+cleanup
+exit 0
